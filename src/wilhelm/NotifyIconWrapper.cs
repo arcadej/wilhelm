36a75719d26d560356b263b148ff7cee2ed3cee9
arcadej <kmasuda@scc-kk.co.jp>
2016-03-04 17:43:29 +0900

    タスクトレイアイコン生成

---
+++ src/wilhelm/NotifyIconWrapper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace wilhelm
+{
+    public partial class NotifyIconWrapper : Component
+    {
+        public NotifyIconWrapper()
+        {
+            InitializeComponent();
+        }
+
+        public NotifyIconWrapper(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// コンテキストメニュー "表示" を選択したとき呼ばれます。
+        /// </summary>
+        /// <param name="sender">呼び出し元オブジェクト</param>
+        /// <param name="e">イベントデータ</param>
+        private void toolStripMenuItem_Open_Click(object sender, EventArgs e)
+        {
+            // MainWindow を生成、表示
+            var wnd = new MainWindow();
+            wnd.Show();
+        }
+
+        /// <summary>
+        /// コンテキストメニュー "終了" を選択したとき呼ばれます。
+        /// </summary>
+        /// <param name="sender">呼び出し元オブジェクト</param>
+        /// <param name="e">イベントデータ</param>
+        private void toolStripMenuItem_Exit_Click(object sender, EventArgs e)
+        {
+            // 現在のアプリケーションを終了
+            Application.Current.Shutdown();
+        }
+    }
+}
